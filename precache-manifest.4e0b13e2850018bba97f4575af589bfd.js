36b90757dd9d12539002926617f8eda35f89a1de
supersatchel <34499356+satchelsmidt@users.noreply.github.com>
2019-11-11 21:03:41 -0800

    Updates

---
+++ precache-manifest.4e0b13e2850018bba97f4575af589bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c177017f1f2382addf32c2b9739a15a1",
+    "url": "/satchelsmidt/React_Memory_Game/index.html"
+  },
+  {
+    "revision": "a0b9f67d7972f47a0ee6",
+    "url": "/satchelsmidt/React_Memory_Game/static/css/main.f4d2e5b2.chunk.css"
+  },
+  {
+    "revision": "b44899efa66fade97188",
+    "url": "/satchelsmidt/React_Memory_Game/static/js/2.bd842754.chunk.js"
+  },
+  {
+    "revision": "a0b9f67d7972f47a0ee6",
+    "url": "/satchelsmidt/React_Memory_Game/static/js/main.81c8af07.chunk.js"
+  },
+  {
+    "revision": "0bdcaadd1e94545913fd",
+    "url": "/satchelsmidt/React_Memory_Game/static/js/runtime-main.a6c77550.js"
+  }
+]);
